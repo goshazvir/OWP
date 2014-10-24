18754f0a4e7cd18b50f164e71b4a41f22348fd5a
Georgiy <goshazvir@gmail.com>
2014-10-24 14:01:32 +0300

    init

--- js/common.js
+++ js/common.js
@@ -1,3 +1,35 @@
 head.ready(function() {
+	smoothScroll.init({
+	    offset: 68, // Integer. How far to offset the scrolling anchor location in pixels
+
+	});
+
+	$('.js-minislide').flexslider({
+		namespace: "minislide-",
+		smoothHeight: true,
+		animation: "fade",
+		selector: ".js-minislide-in > .minislide-item",
+		directionNav: false,
+		slideshowSpeed: 7000,
+		// controlsContainer: ".slider",
+		useCSS: false,
+		controlNav: false,
+		slideshow: true
+	});
+
+	function onepage(){
+		var windowheight = $(window).height();
+		var slideHeight = $('.js-slide').css('height',windowheight);
+	}
+	if ($(".js-slide").length > 0){
+	  onepage();
+	}
+
+	// RESIZE
+	$(window).resize(function(){
+	  if ($(".js-slide").length > 0){
+	    onepage();
+	  }
+	});
 
 });
